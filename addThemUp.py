cebfe456ee92b8a002e714e7242c3c62d9c916d2
Hamidul Hoque Masum <147068671+hamidulhoque11@users.noreply.github.com>
2026-09-19 03:03:47 +0600

    Create addThemUp.py

---
+++ addThemUp.py
@@ -0,0 +1,9 @@
+'''
+Read two integer variables, calculate their sum, and print it.
+Input
+The input will contain two integers A and B (−20000000<A,B<20000000
+Output
+Print the sum of the two integers.
+'''
+a, b = map(int, input().split())
+print(a + b)
